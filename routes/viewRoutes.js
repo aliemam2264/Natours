33fe9234e8b20f55aa00a31ff14d8774fbbd5724
Ali Emam <aliemam2264@gmail.com>
2025-11-05 15:08:24 +0200

    fix some bugs

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.viewAllTours,
-);
+router.get('/', authController.isLoggedIn, viewsController.viewAllTours);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.viewTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
